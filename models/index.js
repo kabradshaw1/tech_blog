21fbd15f23abf234b727487cad186f8725cf4d87
kabradshaw1 <itllbok82@gmail.com>
2022-10-27 13:39:07 -0400

    updated models and handlebars

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Post = require('./Post');
 const User = require('./User');
 
+// create associations
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
@@ -10,4 +11,22 @@ Post.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
 module.exports = { User, Post, Comment };
